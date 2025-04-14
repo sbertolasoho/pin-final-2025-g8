4bc89a53ee35d89100b4dd5f990a3b2845fe6eda
Santiago Bertola <santiago.bertola@soho.lat>
2025-04-14 06:02:44 +0000

    fix for exporter

--- modules/monitoring/service_monitor.tf
+++ modules/monitoring/service_monitor.tf
@@ -15,9 +15,12 @@ resource "kubernetes_manifest" "nginx_service_monitor" {
           app = "nginx"
         }
       }
+      namespaceSelector = {
+        matchNames = ["monitoring"]
+      }
       endpoints = [
         {
-          port     = "http"
+          port     = "metrics"
           interval = "15s"
           path     = "/metrics"
         }
@@ -25,8 +28,44 @@ resource "kubernetes_manifest" "nginx_service_monitor" {
     }
   }
 
-    depends_on = [
+  depends_on = [
     helm_release.kube_prometheus_stack,
     null_resource.wait_for_crds
-    ]
+  ]
+}
+
+resource "kubernetes_manifest" "nginx_service_monitor_default" {
+  manifest = {
+    apiVersion = "monitoring.coreos.com/v1"
+    kind       = "ServiceMonitor"
+    metadata = {
+      name      = "nginx-monitor-default"
+      namespace = "monitoring"   # Se despliega en el namespace de Prometheus
+      labels = {
+        release = "kube-prometheus-stack"
+      }
+    }
+    spec = {
+      selector = {
+        matchLabels = {
+          app = "nginx"
+        }
+      }
+      namespaceSelector = {
+        matchNames = ["default"]   # Selecciona servicios en el namespace "default"
+      }
+      endpoints = [
+        {
+          port     = "metrics"
+          interval = "15s"
+          path     = "/metrics"
+        }
+      ]
+    }
+  }
+
+  depends_on = [
+    helm_release.kube_prometheus_stack,
+    null_resource.wait_for_crds
+  ]
 }
